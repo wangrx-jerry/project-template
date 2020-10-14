861758df991c6dd196f0081f099dd2615fb36bde
jerry <550995259@qq.com>
2020-10-14 10:25:21 +0800

    set theme

--- src/views/someFile/index.vue
+++ src/views/someFile/index.vue
@@ -3,8 +3,14 @@
     * 博客首页
 */
 <template>
-    <div class="p15 tc">
-		 some view page
+    <div class="p15 tc common-util">
+		<div class="p15">
+			<el-button type="primary" size="mini" @click="theme('light')">light</el-button>
+			<el-button type="primary" size="mini" @click="theme('dark')">dark</el-button>
+		</div>
+		<div class="text">
+			some view page
+		</div>
     </div>
 </template>
 <script>
@@ -16,8 +22,25 @@ export default {
 	},
 	mounted () {},
 	computed: {},
-	methods: {}
+	methods: {
+		// 换主题
+		theme (type) {
+			// this.$store.commit('upDate', {themeType: type});
+			window.document.documentElement.setAttribute('data-theme', type);
+		}
+	}
 };
 </script>
 <style scoped lang='scss'>
+// @import "@/assets/styles/_handle.scss";
+    // .common-util {
+    //     font-size: 18px;
+    //     @include font_color("font_color1");
+    //     @include background_color("background_color1");
+    //     @include border_color("border_color1");
+	// }
+	// .text{
+	// 	border: 1px solid;
+	// 	@include border_color("border_color1");
+	// }
 </style>
