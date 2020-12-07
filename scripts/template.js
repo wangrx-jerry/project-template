3e77b9b53dab96fa67c7f95f18c55ac8889ace75
jerry <550995259@qq.com>
2020-12-07 21:38:52 +0800

    router脚本

--- scripts/template.js
+++ scripts/template.js
@@ -1,22 +1,48 @@
 module.exports = {
 	vueTemplate: compoenntName => {
 		return `<template>
-  <div class="${compoenntName}">
-	${compoenntName}组件
-  </div>
-</template>
-<script>
-export default {
-	name: '${compoenntName}'
-}
-</script>
-<style lang="scss" scoped>
-.${compoenntName} {
+					<div class="${compoenntName}">
+						${compoenntName}组件
+					</div>
+				</template>
+				<script>
+					export default {
+						name: '${compoenntName}'
+					}
+				</script>
+				<style lang="scss" scoped>
+					.${compoenntName} {
 
-}
-</style>
-`;
+					}
+				</style>
+				`;
+	},
+	routerFileTemplate: (componentPath, compoenntName, routerFolderDirectory) => {
+		const pathArr = componentPath.split('/src'),
+			relativePath = pathArr.slice(-1)[0];
+		return `export default [
+			{
+				path: '${relativePath}',
+				name: '${compoenntName}',
+				component: () => import(/* webpackChunkName: "${routerFolderDirectory.split('/router/').slice(-1)[0]}" */'@${relativePath}'),
+				meta: {
+					title: 'xxx'
+				}
+			}
+		];`;
+	},
+	routerTemplate: (compoenntName) => {
+		return `
+			{
+				path: 'xxx',
+				name: 'xxx',
+				component: () => import(/* webpackChunkName: "${compoenntName}" */'@xxx'),
+				meta: {
+					title: 'xxx'
+				}
+			}
+		`;
 	},
 	entryTemplate: `import Main from './main.vue'
-export default Main`
+					export default Main`
 };
---
+++ src/views/xxx/xxx.vue
@@ -0,0 +1,16 @@
+<template>
+					<div class="xxx">
+						xxx组件
+					</div>
+				</template>
+				<script>
+					export default {
+						name: 'xxx'
+					}
+				</script>
+				<style lang="scss" scoped>
+					.xxx {
+
+					}
+				</style>
+				
